33df21d1cd57b888ce32e6e63dfac898ad3daa4f
Fernando Tipan <tipqnfernando@gmail.com>
2022-02-11 17:40:31 -0500

    version inicial

--- numeros romanos 2/convertidor.cs
+++ numeros romanos 2/convertidor.cs
@@ -4,27 +4,75 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace numeros_romanos_2
+namespace Mecanica
 {
-   public class convertidor
+   public class Convertidor
     {
-        public static string convertirarabicoaromano(int numeconvertirarabicoaromanoroArabico)
+        public static string PlacaVehiculo(string Placa_Vehiculo)
         {
-            var resultado = new StringBuilder();
-            var restante = numeconvertirarabicoaromanoroArabico;
-            if (numeconvertirarabicoaromanoroArabico == 6)
-               resultado .Append("VI");
-            else if (numeconvertirarabicoaromanoroArabico == 5)
-                resultado .Append("V");
-            else if (numeconvertirarabicoaromanoroArabico == 4)
-                resultado. Append("IV");
-            else
-            for (int i = 0; i< numeconvertirarabicoaromanoroArabico; i++  )
+          
+            if (Placa_Vehiculo == "PCQ0111")
             {
-                resultado.Append("I");
+                return "Toyota Yaris 2010";
             }
-            return resultado.ToString();
-        
+            else if (Placa_Vehiculo == "PBX0211")
+            {
+                return "Dfsk C37 2016 ";
+            }
+
+            else if (Placa_Vehiculo == "PXZ0311")
+            {
+                return "Ford Ranger 2010";
+            }
+            else if (Placa_Vehiculo == "PYZ0411")
+            {
+                return "Nissan Sentra 2007";
+            }
+            else
+            {
+                return "No registrado";
+            }
+        }
+
+        public static string MantenimientoVehiculo(int dia , int mes , int anio  )
+        {
+            
+         
+            DateTime date1 = new DateTime(anio, mes, dia);
+            DateTime date2 = DateTime.Today;
+            int result = (date2 - date1).Days;
+
+            if (result > 90)
+            {
+                return "Si debe realizar mantenimiento";
+            }
+            else
+                return "no debe realizar mantenimiento ";
+    
+
+
+           
+
+
+           
+        }
+        public static string Cambiopiezasvehiculo(int dia, int mes, int anio)
+        {
+
+
+            DateTime date1 = new DateTime(anio, mes, dia);
+            DateTime date2 = DateTime.Today;
+            int result = ((date2 - date1).Days)/365;
+            
+            if (result > 5)
+            {
+                return "Realizar cambio de pieza";
+            }
+            else
+                return "no cambiar pieza ";
+
+
+
         }
     }
 }
--- numeros romanos 3/UnitTest1.cs
+++ numeros romanos 3/UnitTest1.cs
@@ -1,30 +1,70 @@
 using FluentAssertions;
-using numeros_romanos_2;
+using Mecanica;
 using Xunit;
 
-namespace numeros_romanos_3
+namespace Mecanica
 {
-    public class UnitTest1
+    public class UnitTestmecanica 
     {
 
 
         [Theory]
-        [InlineData(1, "I")]
-        [InlineData(2, "II")]
-        [InlineData(3, "III")]
-        [InlineData(4, "IV")]
+        [InlineData("PCQ0111", "Toyota Yaris 2010")]
+        [InlineData("PBX0211", "Dfsk C37 2016 ")]
+        [InlineData("PXZ0311", "Ford Ranger 2010")]
+        [InlineData("PYZ0411", "Nissan Sentra 2007")]
 
-        public void Deberiadevolveracodigoromano (int numeroaconvertir, string resultadoEsperado)
+        public void PlacaVehiculo (string Placa_Vehiculo, string resultadoEsperado)
         {
             
             //Arrange
             //  const string resultadoEsperado = "I";
             //const int numeroaconvertir = 1;
             //Act
-            var resultado = convertidor.convertirarabicoaromano(numeroaconvertir);
+            var resultado = Convertidor.PlacaVehiculo(Placa_Vehiculo);
 
             //Aspect
             resultado.Should().Be(resultadoEsperado);
         }
+        [Theory]
+        [InlineData(05,02,2020 ,"Si debe realizar mantenimiento")]
+        [InlineData(06,05,2015, "Si debe realizar mantenimiento")]
+        [InlineData(29,05,2016, "Si debe realizar mantenimiento")]
+        [InlineData(10,02,2022, "no debe realizar mantenimiento ")]
+        [InlineData(16,05,2005, "Si debe realizar mantenimiento")]
+
+        public void Mantenimiento(int dia ,int mes,int anio,string resultadoEsperado)
+        {
+
+            //Arrange
+            //  const string resultadoEsperado = "I";
+            //const int numeroaconvertir = 1;
+            //Act
+            var resultado = Convertidor.MantenimientoVehiculo(dia,mes,anio);
+
+            //Aspect
+            resultado.Should().Be(resultadoEsperado);
+        }
+
+        [Theory]
+        [InlineData(05, 02, 2020, "no cambiar pieza ")]
+        [InlineData(06, 05, 2015, "Realizar cambio de pieza")]
+        [InlineData(29, 01, 2016, "Realizar cambio de pieza")]
+        [InlineData(10, 02, 2022, "no cambiar pieza ")]
+        [InlineData(16, 05, 2005, "Realizar cambio de pieza")]
+
+        public void Cambiopiezas(int dia, int mes, int anio, string resultadoEsperado)
+        {
+
+            //Arrange
+            //  const string resultadoEsperado = "I";
+            //const int numeroaconvertir = 1;
+            //Act
+            var resultado = Convertidor.Cambiopiezasvehiculo(dia, mes, anio);
+
+            //Aspect
+            resultado.Should().Be(resultadoEsperado);
+        }
+
     }
 }
